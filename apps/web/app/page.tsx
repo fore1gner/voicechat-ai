452b1a15cb68fa7dcb9410b6cf0e06227dccb4f0
Emre Altan <emrealtantr@gmail.com>
2025-12-11 04:06:33 +0100

    initiated convex db and be

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,20 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+"use client"
+import {useMutation, useQuery} from "convex/react"
+import {api} from "@workspace/backend/_generated/api"
+import { Button } from "@workspace/ui/components/button";
+
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-        <p>2 + 3 = {add(2, 3)}</p>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <p>apps/web</p>
+      <div className="mt-4 flex item-center flex-col gap-2 justify-center  p-4">
+      {JSON.stringify(users, null, 2)}
+      <Button onClick={() => addUser()}>Add User</Button>
+       </div>
     </div>
-  )
+
+  ) 
 }
+
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,17 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+"use client"
+import {useQuery} from "convex/react"
+import {api} from "@workspace/backend/_generated/api"
+
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-        <p> 5 +5 ={add(5,5)} </p>
-        <Input />
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <p>apps/widget</p>
+      <div className="mt-4 flex item-center flex-col gap-2 justify-center  p-4">
+      {JSON.stringify(users, null, 2)}
+       </div>
     </div>
-  )
+
+  ) 
 }
+
