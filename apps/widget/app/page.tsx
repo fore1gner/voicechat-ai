34e82c7840c867f2c2ecb947f72a31bd70ca6781
Emre Altan <emrealtantr@gmail.com>
2025-12-27 23:01:01 +0300

    09: widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,29 +1,18 @@
-"use client"
-import { useVapi } from "@/modules/widget/hooks/use-vapi";
-import { Button } from "@workspace/ui/components/button";
+"use client";
 
-export default function Page() {
-  const {
-        isConnected,
-        isConnecting,
-        isSpeaking,
-        transcript,
-        startCall,
-        endCall,
-  } = useVapi();
+import { use } from "react";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <Button onClick={() => startCall()}>Start Call</Button>
-            <Button onClick={() => endCall()} variant={"destructive"}>End Call</Button>
-            <p>is Connected {`${isConnected}`}</p>
-            <p>is Connecting {`${isConnecting}`}</p>
-            <p>is Speaking {`${isSpeaking}`}</p>
-            <pre>{JSON.stringify(transcript, null, 2)}</pre>
-    </div>
-    
-
-
-  ) 
+interface Props {
+  searchParams: Promise<{
+    organizationId: string;
+  }>;
 }
+
+const Page = ({ searchParams }: Props) => {
+  const { organizationId } = use(searchParams);
+  return <WidgetView organizationId={organizationId} />;
+};
+
+export default Page;
 
---
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@workspace/ui/components/button";
+import { HomeIcon, InboxIcon } from "lucide-react";
+import { cn } from "@workspace/ui/lib/utils";
+
+export const WidgetFooter = () => {
+    const screen = "selection";
+
+    return (
+        <footer className="flex items-center justify-between border-t bg-background">
+            <Button
+            className="h-14 flex-1 rounded-none"
+            variant="ghost"
+            size="icon"
+            onClick={() => {}}>
+                <HomeIcon
+                    className={cn("size-5", screen === "selection" && "text-primary")}
+                />
+                <InboxIcon
+                    className={cn("size-5", screen === "inbox" && "text-primary")}/>
+            </Button>
+        </footer>
+    );
+};
