2a725038e66e08dc8f48dc7b350c21b1118b9d21
Emre Altan <emrealtantr@gmail.com>
2026-01-24 20:50:28 +0100

    13: conversations

---
+++ apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { ChevronRightIcon, MessageSquareTextIcon } from "lucide-react";
+import { WidgetHeader } from "../components/widget-header";
+import { Button } from "@workspace/ui/components/button";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { contactSessionIdAtomFamily, conversationIdAtom, errorMessageAtom, organizationIdAtom, screenAtom } from "../../atoms/widget-atoms";
+import { useMutation } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { useState } from "react";
+import { set } from "zod";
+export const WidgetSelectionScreen = () => {
+
+    const setScreen= useSetAtom(screenAtom);
+    const setErrorMessage = useSetAtom(errorMessageAtom);
+    const setConversationId = useSetAtom(conversationIdAtom);
+    const organizationId = useAtomValue(organizationIdAtom);
+    const contactSessionId = useAtomValue(
+        contactSessionIdAtomFamily(organizationId || ""));
+
+    const createConversation = useMutation(api.public.conversations.create);
+    const [isPending, setIsPending] = useState(false);
+
+
+    const handleNewConversation = async () => {
+        setIsPending(true);
+        if(!organizationId){
+            setScreen("error");
+            setErrorMessage("Organization ID is missing.");
+            return;
+        }
+        if(!contactSessionId){
+            setScreen("auth");
+            return;
+        }
+        try {
+            const conversationId = await createConversation({
+                contactSessionId,
+                organizationId,
+            });
+
+            setConversationId(conversationId);
+            setScreen("chat");
+        } catch  {
+            setScreen("auth");
+        } finally {
+            setIsPending(false);
+        }
+    };
+    return (
+        <>
+            <WidgetHeader>
+                <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+                    <p className="text-3xl">
+                        Hi there! 👋
+                    </p>
+                    <p className="text-lg">
+                        Let&apos;s get you started.
+                    </p>
+                </div>
+            </WidgetHeader>
+            <div className="flex flex-1 flex-col items-center gap-y-4 p-4 overflow-y-auto">
+                <Button
+                className="h-16 w-full justify-between"
+                variant="outline"
+                onClick={handleNewConversation}
+                disabled={isPending}>
+                    
+                    <div className="flex items-center gap-x-2">
+                        <MessageSquareTextIcon className="size-4" />
+                        <span>Start a Chat</span>
+                    </div> 
+                    <ChevronRightIcon/>   
+                </Button>
+            </div>
+        </>
+    );
+};
